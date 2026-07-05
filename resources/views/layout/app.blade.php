e026259369fa772e509d45b79eaf6e812d1dd30f
Nihad <nihad.nemetli@gmail.com>
2026-07-05 11:56:27 +0400

    upd: layout upated

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -3,12 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title')</title>
+    <title>@yield('title', 'Laravel Shop')</title>
 </head>
 <body>
     @include('partials.header')
 
-    @yield('content')
+    <main>
+        @yield('content')
+    </main>
 
     @include('partials.footer')
 </body>
