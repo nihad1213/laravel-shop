bb8083cc3f44963e862379e0246e7643562a8e8f
Nihad <nihad.nemetli@gmail.com>
2026-07-11 12:33:15 +0400

    new: vue installed and added app.blade.php file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,14 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Laravel Shop')</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
+
 <body>
-    @include('layouts.partials.header')
-
-    <main>
-        @yield('content')
-    </main>
-
-    @include('layouts.partials.footer')
+    <div>
+        @include('layouts.partials.header')
+    
+            <main>
+                @yield('content')
+            </main>
+    
+        @include('layouts.partials.footer')
+    </div>
 </body>
 </html>
