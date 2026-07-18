d3a3975cc01dd4e2941c49de6ac10a5abda4a59b
Nihad <nihad.nemetli@gmail.com>
2026-07-18 14:07:22 +0400

    del: file deleted, unnecessary use removed

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Services\HomeService;
-use Illuminate\Http\Request;
 use Inertia\Response;
 
 class HomeController extends Controller
