4c7cc0fedd0303c6d8dd68fe7ad341698d52fa3e
Nihad <nihad.nemetli@gmail.com>
2026-07-18 13:46:34 +0400

    upd: php files fixed, intertiajs added

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,17 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'Laravel Shop')</title>
+
+    <!-- Vite Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    @inertiaHead
+</head>
+
+<body>
+    @inertia
+</body>
+</html>
