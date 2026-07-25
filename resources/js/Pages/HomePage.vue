a0405ed2f11bfc102feb8cd3e5acf0c294f9daad
Nihad <nihad.nemetli@gmail.com>
2026-07-25 13:23:28 +0400

    new: HomePage created

--- resources/js/Pages/HomePage.vue
+++ resources/js/Pages/HomePage.vue
@@ -4,9 +4,62 @@ import MainLayout from '@/Layouts/MainLayout.vue';
 
 <template>
     <MainLayout>
-        <h1 class="text-3xl font-bold underline">
-            Hello world!
-        </h1>
-        <p>Welcome to the Laravel Shop! Inertia</p>
+        <div class="mt-10 mb-10 align-content-center">
+            <div class="mb-10">
+                <div class="mb-6">
+                    <div
+                        class="bg-[#2C2A4A] py-3.5 pl-6 pr-10 inline-block max-w-full"
+                        style="clip-path: polygon(0 0, calc(100% - 28px) 0, 100% 50%, calc(100% - 28px) 100%, 0 100%);"
+                    >
+                        <h1 class="text-lg sm:text-xl font-bold text-white whitespace-nowrap">
+                            This Week's Best Selling Vinyl Records &amp; CDs
+                        </h1>
+                    </div>
+                </div>
+                <!-- Carousel will be here -->
+            </div>
+
+            <div class="mb-10">
+                <div class="mb-6">
+                    <div
+                        class="bg-[#2C2A4A] py-3.5 pl-6 pr-10 inline-block max-w-full"
+                        style="clip-path: polygon(0 0, calc(100% - 28px) 0, 100% 50%, calc(100% - 28px) 100%, 0 100%);"
+                    >
+                        <h1 class="text-lg sm:text-xl font-bold text-white whitespace-nowrap">
+                            This Week's Most Valuable Vinyl Records &amp; CDs
+                        </h1>
+                    </div>
+                </div>
+                <!-- Carousel will be here -->
+            </div>
+
+            <div class="mb-10">
+                <div class="mb-6">
+                    <div
+                        class="bg-[#2C2A4A] py-3.5 pl-6 pr-10 inline-block max-w-full"
+                        style="clip-path: polygon(0 0, calc(100% - 28px) 0, 100% 50%, calc(100% - 28px) 100%, 0 100%);"
+                    >
+                        <h1 class="text-lg sm:text-xl font-bold text-white whitespace-nowrap">
+                            This Week's Most Collected Vinyl Records &amp; CDs
+                        </h1>
+                    </div>
+                </div>
+                <!-- Carousel will be here -->
+            </div>
+
+            <div class="mb-10">
+                <div class="mb-6">
+                    <div
+                        class="bg-[#2C2A4A] py-3.5 pl-6 pr-10 inline-block max-w-full"
+                        style="clip-path: polygon(0 0, calc(100% - 28px) 0, 100% 50%, calc(100% - 28px) 100%, 0 100%);"
+                    >
+                        <h1 class="text-lg sm:text-xl font-bold text-white whitespace-nowrap">
+                            Blogs
+                        </h1>
+                    </div>
+                </div>
+                <!-- Blog posts will be here -->
+            </div>
+        </div>
     </MainLayout>
 </template>
