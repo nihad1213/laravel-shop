1bf4361a028ef3b336331b16077722bf3ca02f59
Nihad <nihad.nemetli@gmail.com>
2026-08-08 16:01:24 +0400

    new: Inertia settings changed

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,19 +6,27 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Services\Auth\LoginFormShowService;
+use App\Services\Auth\LoginService;
 use App\Services\Auth\RegisterFormShowService;
+use Illuminate\Http\RedirectResponse;
 use Inertia\Response;
 
 class AuthController extends Controller
 {
     public function __construct(
         private readonly LoginFormShowService $loginFormShowService,
-        private readonly RegisterFormShowService $registerFormShowService
+        private readonly RegisterFormShowService $registerFormShowService,
+        private readonly LoginService $loginService,
     ){}
 
     public function loginFormShow(): Response
     {
         return $this->loginFormShowService->showLoginForm();
+    }
+
+    public function login(): RedirectResponse
+    {
+        return $this->loginService->login();
     }
 
     public function registerFormShow(): Response
