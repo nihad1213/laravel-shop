ba2c9af2e4a51d447c400fb4e9cf8be857683f4e
Nihad <nihad.nemetli@gmail.com>
2026-08-08 14:20:06 +0400

    upd: Header mobile part removed due to simplicty

--- resources/js/Components/Header.vue
+++ resources/js/Components/Header.vue
@@ -128,54 +128,5 @@ const cartItemCount = ref(3)
         </Link>
       </div>
     </nav>
-
-    <!-- Mobile Navigation Drawer -->
-    <Dialog class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
-      <div class="fixed inset-0 z-50"></div>
-      <DialogPanel class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-[#2C2A4A] p-6 sm:max-w-sm sm:ring-1 sm:ring-[#DABFFF]/10">
-        <div class="flex items-center justify-between">
-          <a href="#" class="-m-1.5 p-1.5">
-            <span class="sr-only">Your Company</span>
-            <img class="h-8 w-auto" src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500" alt="" />
-          </a>
-          <button type="button" class="-m-2.5 rounded-md p-2.5 text-[#907AD6]" @click="mobileMenuOpen = false">
-            <span class="sr-only">Close menu</span>
-            <XMarkIcon class="size-6" aria-hidden="true" />
-          </button>
-        </div>
-        <div class="mt-6 flow-root">
-          <div class="-my-6 divide-y divide-[#DABFFF]/10">
-            <div class="space-y-2 py-6">
-              <Disclosure as="div" class="-mx-3" v-slot="{ open }">
-                <DisclosureButton class="flex w-full items-center justify-between rounded-lg py-2 pr-3.5 pl-3 text-base/7 font-semibold text-white hover:bg-[#DABFFF]/10">
-                  Product
-                  <ChevronDownIcon :class="[open ? 'rotate-180' : '', 'size-5 flex-none']" aria-hidden="true" />
-                </DisclosureButton>
-                <DisclosurePanel class="mt-2 space-y-2">
-                  <DisclosureButton v-for="item in [...products, ...callsToAction]" :key="item.name" as="a" :href="item.href" class="block rounded-lg py-2 pr-3 pl-6 text-sm/7 font-bold text-white hover:bg-[#DABFFF]/10">{{ item.name }}</DisclosureButton>
-                </DisclosurePanel>
-              </Disclosure>
-              <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-bold text-white hover:bg-[#DABFFF]/10">Features</a>
-              <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-bold text-white hover:bg-[#DABFFF]/10">Marketplace</a>
-              <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-bold text-white hover:bg-[#DABFFF]/10">Company</a>
-              
-              <!-- Mobile Cart Link -->
-              <a href="#" class="-mx-3 flex items-center justify-between rounded-lg px-3 py-2 text-base/7 font-bold text-white hover:bg-[#DABFFF]/10">
-                <span class="flex items-center gap-x-2">
-                  <ShoppingBagIcon class="size-5 text-[#907AD6]" aria-hidden="true" />
-                  Basket
-                </span>
-                <span v-if="cartItemCount > 0" class="rounded-full bg-[#907AD6] px-2 py-0.5 text-xs text-white">
-                  {{ cartItemCount }}
-                </span>
-              </a>
-            </div>
-            <div class="py-6">
-              <a href="#" class="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-bold text-white hover:bg-[#DABFFF]/10">Log in</a>
-            </div>
-          </div>
-        </div>
-      </DialogPanel>
-    </Dialog>
   </header>
 </template>
